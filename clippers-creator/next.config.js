ff3918bc37b6a1084f764194c70e849a5ca6f83d
AzibapuV <azibapugabriel.gmail.com>
2026-07-26 20:48:02 +0100

    Fix build: mark ffmpeg/ffprobe as external server packages

--- clippers-creator/next.config.js
+++ clippers-creator/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["fluent-ffmpeg", "@ffprobe-installer/ffprobe"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" }
